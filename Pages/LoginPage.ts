681c76dd3fc08e38e895c77287e2cf55ad0654d4
Bhupesh Gunda <gundabhupesh111@gmail.com>
2025-12-09 21:05:12 -0500

    added spec files and necessary Pages

---
+++ Pages/LoginPage.ts
@@ -0,0 +1,28 @@
+import { Page, Locator } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class LoginPage extends BasePage {
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  readonly errorMessage: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.emailInput = page.locator('#user-name');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.locator('#login-button');
+    this.errorMessage = page.locator('[data-test="error"]');
+  }
+
+  async login(username: string, password: string) {
+    await this.emailInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+
+  async getErrorMessage(): Promise<string | null> {
+    return await this.errorMessage.textContent();
+  }
+}
+3.
